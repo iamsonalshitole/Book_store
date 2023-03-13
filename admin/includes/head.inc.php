bc098d64a362e0325da1ecf2a05811836a483b59
Sonal <shitolesonal2001@gmail.com>
2023-03-13 11:51:41 +0530

    bookstore update

---
+++ admin/includes/head.inc.php
@@ -0,0 +1,5 @@
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>I2IT Book Store</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="default.css" rel="stylesheet" type="text/css" />
